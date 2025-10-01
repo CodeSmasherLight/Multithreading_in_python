1895bf571ebbb23a74da3faafca36d8270353f9d
Satyam Singh <singhsatyam8383@gmail.com>
2025-10-01 23:03:08 +0530

    update thread.py

--- thread.py
+++ thread.py
@@ -6,22 +6,23 @@ def square():
         n * n
         time.sleep(1) 
 
+if __name__ == "__main__":
 
-threads = []
-thread_count = 10
+    threads = []
+    thread_count = 10
 
-# creating threads
-for i in range(thread_count):
-    t = Thread(target=square) 
-    threads.append(t)
-    
-# start all threads
-for t in threads:
-    t.start()
+    # creating threads
+    for i in range(thread_count):
+        t = Thread(target=square) 
+        threads.append(t)
+        
+    # start all threads
+    for t in threads:
+        t.start()
 
-# join thread, so basically we are waiting for all threads to complete
-for t in threads:
-    t.join()
+    # join thread, so basically we are waiting for all threads to complete
+    for t in threads:
+        t.join()
 
-print("All threads completed.")    
+    print("finish main.")    
 
